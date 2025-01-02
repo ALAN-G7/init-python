744ac5d833fbbf20bf52d222fcac167615ffae42
ALAN-G7 <alan.hanaffi17@gmail.com>
2025-01-02 17:06:38 +0300

    ajout des fichier

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 print("j'apprend Pytohn!")
 calcul=17+35*5
 print(f"Resultat {calcul}")
-print(12)
+nom="alan"
+age="27"
+print(f"je m'appelle {nom} et j'ai {age} ")
+nom="goblin"
+age= 17
+taille = 1.85 
+si_etudiant= True
+si_sfd= False
+
+print(f"resultat {nom} {age} {taille} {si_etudiant} {si_sfd}")
+fruits=['pomme', 'anane', 'orange']
+fruits[0]='kida'
+print(fruits)
+ 
+info={
+    "poids":"13",
+    "origine":"madagascar"
+}
+info['nom']="alan"
+del info["origine"]
+info.keys()
+print(info)
+ 
+fruit ={
+  "pomme":"rouge",
+  "banane":"jaune",
+  "orange":"orange"
+ }
+cles=fruit.values()
+print(cles)
+fruit["kiwi"]="vert"
+fruit["banane"]="couleur_banane"
